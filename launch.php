6a91bfa51520052cec3e2adbdf2a77d7b16e4a07
cubeman329 <112988961+cubeman329@users.noreply.github.com>
2022-10-22 20:53:25 -0500

    Create launch.php

---
+++ launch.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>
+    Launch
+    </title>
+    <style>
+      
+    </style>
+  </head>
+  <body>
+    <h1>
+      IDs not implemented <u>yet</u>
+    </h1>
+    <a href="Gaiges web/Home/">
+      <button>Launch</button>
+    </a>
+  </body>
+</html>
