1d525a2c34ec558b2574434b9ee7a97bd4870bfb
cubeman329 <112988961+cubeman329@users.noreply.github.com>
2022-11-18 04:00:31 +0000

    Update

--- Gaiges web/Soundboard/soundboard.js
+++ Gaiges web/Soundboard/soundboard.js
@@ -1,2 +1,38 @@
 const boom = new Audio();
 boom.src = './boom.mp3';
+
+const emotionaldamage = new Audio();
+emotionaldamage.src = "./emotionaldamage.mp3"
+
+const bird = new Audio();
+bird.src = "./bird.mp3";
+
+const fart = new Audio();
+fart.src = "fart.mp3";
+
+const bababooey = new Audio();
+bababooey.src = "bababooey.mp3";
+
+const notif = new Audio();
+notif.mp3 = "./notif";
+
+const bruh = new Audio();
+bruh.src = "./bruh.mp3";
+
+const heheheha = new Audio();
+heheheha.src = "./heheheha.mp3";
+
+const mm = new Audio();
+mm.src = './MUSIC MAN.mp3';
+
+const bonk = new Audio();
+bonk.src = './mm.mp3';
+
+const fart3 = new Audio();
+fart3.src = './fart3.mp3';
+
+const hwg = new Audio();
+hwg.src = './hwg.mp3';
+
+const omg = new Audio();
+omg.src = './oh mah gahd.mp3';
